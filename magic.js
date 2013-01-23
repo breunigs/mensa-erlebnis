4b6919f86cbdd87105dfec724e9dedc7990f4289
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-01-23 18:54:04 +0100

    initial commit

---
+++ magic.js
@@ -0,0 +1,122 @@
+function changeAufgang() {
+  var a = $('input[name=aufgang]:checked').val();
+  if(!a) return;
+  a = a.toLowerCase();
+  $("#aufgang-details").children().trigger("collapse");
+  $(".details-aufgang-" + a).trigger("expand");
+}
+
+function getText() {
+  var aufgang = $('input[name=aufgang]:checked').val();
+  var text = "Liebes StuWe,\n\nich war heute ";
+  text += {"AB": "beim Buffet", "D": "bei D", "E": "an Aufgang E"}[aufgang];
+
+  if(aufgang == "AB") {
+    var kats = $('input[name=ab-kategorie]:checked');
+    if(kats.length == 0) {
+      text += ". ";
+    } else if(kats.length == 1) {
+      text += " und habe mich bei den " + $(kats.first()).val();
+      text += " bedient.";
+    } else {
+      text += " und habe mich bei den " + $(kats.first()).val();
+      $.each(kats.slice(1,-1), function(ind, x) {
+	text += ", " + $(x).val();
+      });
+      text += " und " + $(kats.last()).val() + " bedient.";
+    }
+  }
+
+  if(aufgang == "D") {
+    var gericht = $('input[name=d-gericht]:checked').val();
+    if(gericht) {
+      text += " und habe das ";
+      text += gericht == "veget" ? "vegetarische Gericht" : "Fleisch/Fischgericht";
+      text += " (";
+      text += data["d"][gericht] + ") gegessen. ";
+    } else {
+      text += ". "
+    }
+  }
+
+  if(aufgang == "E") {
+    var gericht = $('input[name=e-gericht]:checked').val();
+    if(gericht) {
+      text += " und hatte das ";
+      text += gericht == "veget" ? "vegetarische Gericht" : "Fleisch/Fischgericht";
+      text += " (";
+      text += data["e"][gericht] + ") ";
+    } else {
+      text += " und hatte kein Hauptgericht "
+    }
+
+    var beilagen = $('input[name=e-beilage]:checked');
+    if(beilagen.length == 0) {
+      text += "ohne Beilagen."
+    } else if(beilagen.length == 1) {
+      text += "mit " + $(beilagen.first()).val() + ".";
+    } else {
+      text += "mit " + $(beilagen.first()).val();
+      $.each(beilagen.slice(1,-1), function(ind, x) {
+	text += ", " + $(x).val();
+      });
+      text += " und " + $(beilagen.last()).val() + ".";
+    }
+  }
+
+  text += "\n\n";
+
+  var stimmung = $('input[name=stimmung]:checked').val();
+  if(stimmung == "Neutral") text += "Das Essen bewerte ich neutral."
+  if(stimmung == "Lob") text += "Das Essen fand ich sehr gut und spreche mein Lob aus."
+  if(stimmung == "Tadel") text += "Das Essen war nicht so gut und ich muss tadeln."
+
+  var msg = $("#mitteilung").val().replace(/^\s+|\s+$/, "");
+  if(msg !== "") {
+    text += " Außerdem möchte ich noch sagen: " + msg;
+  }
+
+  text += "\n\nViele Grüße"
+  return text;
+}
+
+
+var submitInProgress = false;
+
+$(document).ready(function() {
+  $("input[type='radio']").bind("change", function(event) {
+    if(event.target.name === "aufgang") changeAufgang();
+  });
+
+  changeAufgang(); // for reloads
+
+  $("form").submit(function(e) {
+    if(submitInProgress) return false;
+    submitInProgress = true;
+    $.mobile.showPageLoadingMsg();
+    $('[type="submit"]').button('disable');
+
+    var i = $("iframe").contents();
+    i.find("form").attr("onsubmit", "");
+    i.find("select").val("01|Essen & Trinken");
+    $("iframe").contents().find("input[name=zv_action]").val("SW_SEND_FEEDBACK");
+    //i.find("input[name=SW_LOB_SUBJECT]").val("Mein Mensa-Erlebnis");
+    i.find("textarea[name=SW_LOB_COMMENT]").val(getText());
+    $("iframe").one("load", function() {
+      $.mobile.hidePageLoadingMsg();
+      alert("Erlebnis gesendet!");
+      $("form")[0].reset();
+      $.mobile.silentScroll(0);
+      submitInProgress = false;
+      $('[type="submit"]').button('enable');
+      //$("iframe").attr("src", "form.html");
+    });
+    // actually submit the form
+    i.find("form").submit();
+
+    return false;
+  });
+
+
+  $(document.body).append('<iframe id="stwform" src="form.html"></iframe>');
+});
---
+++ updater.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# The cryptic links are actually base16 encoded, with CRB and CRE guard
+# blocks in the beginning/end. So, the links are like this:
+# …zv_portal/zv?px=CRB«here be base16»CRE
+
+plan = "http://www.studentenwerk.uni-heidelberg.de/zv_portal/zv?zv_action=CH_PAGE&zv_value=SWD_HD_Essen_HD_Speiseplan"
+form = "http://www.studentenwerk.uni-heidelberg.de/zv_portal/zv?zv_action=CH_PAGE&zv_value=SWD_HD_Essen_HD_LobTadel"
+save_plan_path = "/home/stefan/public_html/mensa/data.json"
+save_form_path = "/home/stefan/public_html/mensa/form.html"
+
+require "rubygems"
+require "mechanize"
+require "pp"
+require "json"
+
+class String
+  def clean
+    self.downcase.gsub(/[^a-z0-9]/, "")
+  end
+
+  def lesser_clean
+    x = self.gsub(/\((?:[1-9]+,?)+\)$/, "").strip
+    x
+  end
+end
+
+data = {}
+
+a = Mechanize.new
+a.content_encoding_hooks << lambda{|httpagent, uri, response, body_io|
+  response['content-encoding'] = nil if response['content-encoding'] == 'ISO-8859-15'
+}
+
+a.get(plan) do |page|
+  # iterate over table rows
+  page.search("div[id=textbereich]/table").first.search("tr").each do |tr|
+    t = tr.search("td/text()")
+    name = t[0].to_s.lesser_clean
+    aufgang = t[1].to_s.clean
+    if aufgang.size > 4
+      name = [name, t[1].to_s.lesser_clean]
+      aufgang = t[2].to_s.clean
+    end
+    next if aufgang.empty? || name.empty?
+    data[aufgang] ||= []
+    data[aufgang] << name
+  end
+end
+
+f = {}
+f[:d] = {}
+f[:d][:fleisch] = data["d"].select { |x| !x.include?("(veget.)") }.first
+f[:d][:veget] = data["d"].select { |x| x.include?("(veget.)") }.first
+
+f[:e] = {}
+f[:e][:fleisch] = data["e"][1..2].select { |x| !x.include?("(veget.)") }.first
+f[:e][:veget] = data["e"][1..2].select { |x| x.include?("(veget.)") }.first
+f[:e][:beilagen] = [data["e"][0]] + data["e"][3..999]
+
+File.open(save_plan_path, "w") do |d|
+  d.write("var data = ")
+  d.write(f.to_json)
+end
+
+
+# get the form, so we don’t need to worry about the ID-magic
+page = a.get(form)
+File.open(save_form_path, "w") do |d|
+  d.write(%(<!DOCTYPE html>\n))
+  d.write(%(<html><head><meta charset="ISO-8859-15"></head><body>))
+  d.write(page.search("form[2]").to_s)
+  d.write(%(</body>))
+end

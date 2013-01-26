0feac8d5269b7ec5a6d4e6641430d3d5b8769e5a
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-01-26 09:48:52 +0100

    handle info in saturday and sunday

--- updater.rb
+++ updater.rb
@@ -14,6 +14,7 @@ require "rubygems"
 require "mechanize"
 require "pp"
 require "json"
+require "date"
 
 class String
   def clean
@@ -26,44 +27,45 @@ class String
   end
 end
 
-data = {}
+unless Date.today.saturday? || Date.today.sunday?
+  data = {}
 
-a = Mechanize.new
-a.content_encoding_hooks << lambda{|httpagent, uri, response, body_io|
-  response['content-encoding'] = nil if response['content-encoding'] == 'ISO-8859-15'
-}
+  a = Mechanize.new
+  a.content_encoding_hooks << lambda{|httpagent, uri, response, body_io|
+    response['content-encoding'] = nil if response['content-encoding'] == 'ISO-8859-15'
+  }
 
-a.get(plan) do |page|
-  # iterate over table rows
-  page.search("div[id=textbereich]/table").first.search("tr").each do |tr|
-    t = tr.search("td/text()")
-    name = t[0].to_s.lesser_clean
-    aufgang = t[1].to_s.clean
-    if aufgang.size > 4
-      name = [name, t[1].to_s.lesser_clean]
-      aufgang = t[2].to_s.clean
+  a.get(plan) do |page|
+    # iterate over table rows
+    page.search("div[id=textbereich]/table").first.search("tr").each do |tr|
+      t = tr.search("td/text()")
+      name = t[0].to_s.lesser_clean
+      aufgang = t[1].to_s.clean
+      if aufgang.size > 4
+        name = [name, t[1].to_s.lesser_clean]
+        aufgang = t[2].to_s.clean
+      end
+      next if aufgang.empty? || name.empty?
+      data[aufgang] ||= []
+      data[aufgang] << name
     end
-    next if aufgang.empty? || name.empty?
-    data[aufgang] ||= []
-    data[aufgang] << name
+  end
+
+  f = {}
+  f[:d] = {}
+  f[:d][:fleisch] = data["d"].select { |x| !x.include?("(veget.)") }.first
+  f[:d][:veget] = data["d"].select { |x| x.include?("(veget.)") }.first
+
+  f[:e] = {}
+  f[:e][:fleisch] = data["e"][1..2].select { |x| !x.include?("(veget.)") }.first
+  f[:e][:veget] = data["e"][1..2].select { |x| x.include?("(veget.)") }.first
+  f[:e][:beilagen] = [data["e"][0]] + data["e"][3..999]
+
+  File.open(save_plan_path, "w") do |d|
+    d.write("var data = ")
+    d.write(f.to_json)
   end
 end
-
-f = {}
-f[:d] = {}
-f[:d][:fleisch] = data["d"].select { |x| !x.include?("(veget.)") }.first
-f[:d][:veget] = data["d"].select { |x| x.include?("(veget.)") }.first
-
-f[:e] = {}
-f[:e][:fleisch] = data["e"][1..2].select { |x| !x.include?("(veget.)") }.first
-f[:e][:veget] = data["e"][1..2].select { |x| x.include?("(veget.)") }.first
-f[:e][:beilagen] = [data["e"][0]] + data["e"][3..999]
-
-File.open(save_plan_path, "w") do |d|
-  d.write("var data = ")
-  d.write(f.to_json)
-end
-
 
 # get the form, so we don’t need to worry about the ID-magic
 page = a.get(form)
